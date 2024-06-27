7ae4c843a14faa100dd59971397742af1cf9bf60
Giang <nguyenvietgiang1110@gmail.com>
2024-06-27 14:27:02 +0700

    JenkinsAutoRun

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
-import Login from './components/login';
-import Register from './components/register';
-import CharacterList from './components/CharacterList';
-import CharacterDetail from './components/CharacterDetail';
-import authService from './services/authService';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
+import Login from "./components/login";
+import Register from "./components/register";
+import CharacterList from "./components/CharacterList";
+import CharacterDetail from "./components/CharacterDetail";
+import authService from "./services/authService";
+import "./App.css";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -24,33 +24,79 @@ function App() {
     authService.logout();
     setIsLoggedIn(false);
     // Chuyển hướng đến trang đăng nhập
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <div className="App">
       <header className="App-header">
-          <Link to="/CharacterList">
-            <img src="https://logos-world.net/wp-content/uploads/2022/01/Rick-And-Morty-Logo.png" alt="Logo" width="300" height="150" />
+        <Link to="/CharacterList">
+          <img
+            src="https://logos-world.net/wp-content/uploads/2022/01/Rick-And-Morty-Logo.png"
+            alt="Logo"
+            width="300"
+            height="150"
+          />
+        </Link>
+        {isLoggedIn ? (
+          <div>
+            <Link to="/CharacterList" className="link">
+              Character List
+            </Link>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <Link to="/register" className="link">
+            Register
           </Link>
-          {isLoggedIn ? (
-            <div>
-              <Link to="/CharacterList" className="link">Character List</Link>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-          ) : (
-            <Link to="/register" className="link">Register</Link>
-          )}
+        )}
         <div className="content">
-        <Routes>
-      <Route path="/" element={isLoggedIn ? <Navigate to="/CharacterList" /> : <Navigate to="/login" />} />
-       <Route path="/CharacterList" element={isLoggedIn ? <CharacterList /> : <Navigate to="/login" />} />
-      <Route path="/CharacterDetail/:id" element={isLoggedIn ? <CharacterDetail /> : <Navigate to="/login" />} />
-      <Route path="/register" element={!isLoggedIn ? <Register /> : <Navigate to="/CharacterList" />} />
-      <Route path="/login" element={!isLoggedIn ? <Login /> : <Navigate to="/CharacterList" />} />
-       </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                isLoggedIn ? (
+                  <Navigate to="/CharacterList" />
+                ) : (
+                  <Navigate to="/login" />
+                )
+              }
+            />
+            <Route
+              path="/CharacterList"
+              element={
+                isLoggedIn ? <CharacterList /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              path="/CharacterDetail/:id"
+              element={
+                isLoggedIn ? <CharacterDetail /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                !isLoggedIn ? <Register /> : <Navigate to="/CharacterList" />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                !isLoggedIn ? <Login /> : <Navigate to="/CharacterList" />
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                !isLoggedIn ? <Login /> : <Navigate to="/CharacterList" />
+              }
+            />
+          </Routes>
         </div>
-        <h2><strong>@2023 by Rick and Morty</strong></h2>
+        <h2>
+          <strong>@2023 by Rick and Morty</strong>
+        </h2>
         <h3>Design by Nguyen Viet Giang</h3>
         <h3>--My First React App--</h3>
       </header>
@@ -59,7 +105,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
---
+++ src/components/aboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is a brief description of our company.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
